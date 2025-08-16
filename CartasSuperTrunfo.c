80f8cbee178d294c345a2a511a0ddcfd62ea525a
Zmpra5442 <adoniasmachado01@gmail.com>
2025-08-16 13:04:33 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,8 @@
         int popu, popu2;
         int pontos, pontos2;
         float area, area2;
-        float pib, pib2;
+        float de, de2;
+        float pib, pib2, pibp, pibp2;
 
 printf("Primeiro defina o Estado da sua carta (Uma letra de A a Z):\n", estado);
 scanf("%s", &estado);
@@ -23,14 +24,21 @@ scanf("%d", &popu);
 printf("Escreva o total de pontos turísticos do seu estado:\n", pontos);
 scanf("%d", &pontos);
 
-printf("Escreva o área em km^2 do seu estado:\n", area);
+printf("Escreva o área em (km^2) do seu estado:\n", area);
 scanf("%f", &area);
 
-printf("Por último,escreva o PIB em reais do seu estado:\n", pib);
+printf("Por último,escreva o PIB (em reais) do seu estado:\n", pib);
 scanf("%f", &pib);
 
+printf("Digite a densidade populacional (em hab/km^2) do seu estado:\n", de);
+scanf("%f", &de);
+
+printf("Digite o PIB per capita (em reais da sua cidade:\n", pibp);
+scanf("%f", &pibp);
+
 printf("Estado: %s\n - Código: %s\n - Cidade: %s\n", estado, codigo, ncidade);
-printf("População: %d\n - Quantidade de pontos turísticos: %d\n - Área: %f\n - PIB: %f\n", popu, pontos, area, pib);
+printf("População: %d habitantes\n - Quantidade de pontos turísticos: %d\n - Área: %.2f km^2\n - PIB: %.2f reais\n", popu, pontos, area, pib);
+printf("Densidade demográfica: %.2f hab/km^2\n - PIB per capita: %.2f reais\n", de, pibp);
 
 printf("Vamos cadastrar a sua segunda carta\n");
 
@@ -49,14 +57,21 @@ scanf("%d", &popu2);
 printf("Escreva o total de pontos turísticos do seu estado:\n", pontos2);
 scanf("%d", &pontos2);
 
-printf("Escreva o área em km^2 do seu estado:\n", area2);
+printf("Escreva o área (em km^2) do seu estado:\n", area2);
 scanf("%f", &area2);
 
-printf("Por último,escreva o PIB em reais do seu estado:\n", pib2);
+printf("Por último,escreva o PIB (em reais) do seu estado:\n", pib2);
 scanf("%f", &pib2);
 
+printf("Digite a densidade populacional (em hab/km^2) do seu estado:\n", de);
+scanf("%f", &de2);
+
+printf("Digite o PIB per capita (em reais) da sua cidade:\n", pibp);
+scanf("%f", &pibp2);
+
 printf("Estado: %s\n - Código: %s\n - Cidade: %s\n", estado2, codigo2, ncidade2);
-printf("População: %d\n - Quantidade de pontos turísticos: %d\n - Área: %f\n - PIB: %f\n", popu2, pontos2, area2, pib2);
+printf("População: %d habitantes\n - Quantidade de pontos turísticos: %d\n - Área: %.2f km^2\n - PIB: %.2f reais\n", popu2, pontos2, area2, pib2);
+printf("Densidade demográfica: %.2f hab/km^2\n - PIB per capita: %.2f reais\n", de2, pibp2);
 
 return 0;
 }
